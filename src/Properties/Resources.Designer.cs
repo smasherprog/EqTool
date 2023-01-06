c5ed4543b417f35e8c1a59c9769f014fdfb6504b
Scott Lee <scott.lee@tai-software.com>
2023-01-05 21:14:48 -0800

    added app logging

--- src/Properties/Resources.Designer.cs
+++ src/Properties/Resources.Designer.cs
@@ -64,16 +64,16 @@ namespace EQTool.Properties {
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;
         ///&lt;configuration&gt;
         ///    &lt;startup&gt; 
-        ///        &lt;supportedRuntime version=&quot;v4.0&quot; sku=&quot;.NETFramework,Version=v4.7.2&quot; /&gt;
+        ///        &lt;supportedRuntime version=&quot;v4.0&quot; sku=&quot;.NETFramework,Version=v4.8&quot;/&gt;
         ///    &lt;/startup&gt; 
         ///  &lt;runtime&gt;
         ///    &lt;assemblyBinding xmlns=&quot;urn:schemas-microsoft-com:asm.v1&quot;&gt;
         ///      &lt;dependentAssembly&gt;
-        ///        &lt;assemblyIdentity name=&quot;System.Memory&quot; publicKeyToken=&quot;cc7b13ffcd2ddd51&quot; culture=&quot;neutral&quot; /&gt;
-        ///        &lt;bindingRedirect oldVersion=&quot;0.0.0.0-4.0.1.2&quot; newVersion=&quot;4.0.1.2&quot; /&gt;
+        ///        &lt;assemblyIdentity name=&quot;System.Memory&quot; publicKeyToken=&quot;cc7b13ffcd2ddd51&quot; culture=&quot;neutral&quot;/&gt;
+        ///        &lt;bindingRedirect oldVersion=&quot;0.0.0.0-4.0.1.2&quot; newVersion=&quot;4.0.1.2&quot;/&gt;
         ///      &lt;/dependentAssembly&gt;
         ///      &lt;dependentAssembly&gt;
-        ///  [rest of string was truncated]&quot;;.
+        ///       [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string App {
             get {
