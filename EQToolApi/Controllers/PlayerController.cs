17630af1abc2c4504085ab33c9e8be21c0625db7
Scott Lee <scott.lee@tai-software.com>
2023-02-04 18:53:50 -0800

    updates

--- EQToolApi/Controllers/PlayerController.cs
+++ EQToolApi/Controllers/PlayerController.cs
@@ -29,14 +29,37 @@ namespace EQToolApi.Controllers
         }
 
         [Route("api/player/getbyname")]
-        public Player Get([FromUri] PlayerRequest playerRequest)
+        public Player GetGetByName([FromUri] PlayerRequest playerRequest)
         {
             return dbcontext.Players.FirstOrDefault(a => a.Name == playerRequest.Name && a.Server == playerRequest.Server);
         }
 
-        public void Post([FromBody] PlayerCreate model)
+        [Route("api/player/getbynames"), HttpPost]
+        public List<Player> GetByNames([FromBody] List<PlayerRequest> models)
         {
+            var ret = new List<Player>();
+            foreach (var item in models.GroupBy(a => a.Server))
+            {
+                var names = item.Select(a => a.Name).ToList();
+                ret.AddRange(dbcontext.Players.Where(a => names.Contains(a.Name) && a.Server == item.Key).ToList());
+            }
 
+            return ret;
+        }
+
+        [Route("api/player/upsertplayer"), HttpPost]
+        public void Update([FromBody] Player model)
+        {
+            var p = dbcontext.Players.FirstOrDefault(a => a.Name == model.Name && a.Server == model.Server);
+            if (p == null)
+            {
+                _ = dbcontext.Players.Add(model);
+            }
+            else if (model.Level > p.Level)
+            {
+                p.Level = model.Level;
+            }
+            _ = dbcontext.SaveChanges();
         }
 
         protected override void Dispose(bool disposing)
