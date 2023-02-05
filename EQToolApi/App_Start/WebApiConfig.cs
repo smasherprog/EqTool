5a79598ce7caaa7678c039ffac522ca84bbbc71a
Scott Lee <scott.lee@tai-software.com>
2023-02-04 17:28:26 -0800

    updates

--- EQToolApi/App_Start/WebApiConfig.cs
+++ EQToolApi/App_Start/WebApiConfig.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace EQToolApi
@@ -14,11 +12,13 @@ namespace EQToolApi
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
+            _ = config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
