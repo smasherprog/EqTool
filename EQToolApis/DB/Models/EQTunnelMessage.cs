4ea225406bb46d9bdf9a665b58c0e438c650a70e
Scott Lee <scott.lee@tai-software.com>
2023-03-18 18:50:25 -0700

    index add

--- EQToolApis/DB/Models/EQTunnelMessage.cs
+++ EQToolApis/DB/Models/EQTunnelMessage.cs
@@ -2,7 +2,7 @@
 
 namespace EQToolApis.DB.Models
 {
-    [PrimaryKey(nameof(EQTunnelMessageId)), Index(nameof(DiscordMessageId)), Index(nameof(Server))]
+    [PrimaryKey(nameof(EQTunnelMessageId)), Index(nameof(DiscordMessageId)), Index(nameof(Server)), Index(nameof(AuctionType))]
     public class EQTunnelMessage
     {
         public long EQTunnelMessageId { get; set; }
---
+++ EQToolApis/Migrations/20230319015013_ItemIndexAddTunnel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EQToolApis.Migrations
+{
+    /// <inheritdoc />
+    public partial class ItemIndexAddTunnel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_EQTunnelMessages_AuctionType",
+                table: "EQTunnelMessages",
+                column: "AuctionType");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_EQTunnelMessages_AuctionType",
+                table: "EQTunnelMessages");
+        }
+    }
+}
