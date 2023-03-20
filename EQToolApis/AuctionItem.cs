441a28b82f9822003270f188056a97825e5983df
Scott Lee <scott.lee@tai-software.com>
2023-03-19 21:59:52 -0700

    more changes to page

--- EQToolApis/AuctionItem.cs
+++ EQToolApis/AuctionItem.cs
@@ -1,13 +1,36 @@
-﻿namespace EQToolApis
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EQToolApis
 {
     public class AuctionItem
     {
-        public DateTimeOffset LastSeen { get; set; }
-
+        [MaxLength(64)]
         public string ItemName { get; set; }
 
-        public int Count { get; set; }
+        public DateTimeOffset LastSeen { get; set; }
 
-        public int AveragePrice { get; set; }
+        public int TotalAuctionCount { get; set; }
+
+        public int TotalAuctionAverage { get; set; }
+
+        public int TotalLast30DaysCount { get; set; }
+
+        public int TotalLast30DaysAverage { get; set; }
+
+        public int TotalLast60DaysCount { get; set; }
+
+        public int TotalLast60DaysAverage { get; set; }
+
+        public int TotalLast90DaysCount { get; set; }
+
+        public int TotalLast90DaysAverage { get; set; }
+
+        public int TotalLast6MonthsCount { get; set; }
+
+        public int TotalLast6MonthsAverage { get; set; }
+
+        public int TotalLastYearCount { get; set; }
+
+        public int TotalLastYearAverage { get; set; }
     }
 }
--- EQToolApis/Pages/Index.cshtml.cs
+++ EQToolApis/Pages/Index.cshtml.cs
@@ -33,14 +33,22 @@ namespace EQToolApis.Pages
                 {
                     _AuctionItems = context.EQTunnelAuctionItems
                        .Where(a => a.EQTunnelMessage.AuctionType == AuctionType.WTS)
-                       .GroupBy(a => new { a.EQitem.ItemName, a.EQitem.TotalLast30DaysAverage, a.EQitem.TotalLast30DaysCount, a.EQitem.LastSeen })
-                       .Select(a => new AuctionItem
+                       .GroupBy(a => new AuctionItem
                        {
-                           ItemName = a.Key.ItemName,
-                           AveragePrice = a.Key.TotalLast30DaysAverage,
-                           Count = a.Key.TotalLast30DaysCount,
-                           LastSeen = a.Key.LastSeen
-                       }).ToList()
+                           ItemName = a.EQitem.ItemName,
+                           TotalLast30DaysAverage = a.EQitem.TotalLast30DaysAverage,
+                           TotalLast30DaysCount = a.EQitem.TotalLast30DaysCount,
+                           TotalLast60DaysCount = a.EQitem.TotalLast60DaysCount,
+                           TotalLast60DaysAverage = a.EQitem.TotalLast60DaysAverage,
+                           TotalLast90DaysCount = a.EQitem.TotalLast90DaysCount,
+                           TotalLast90DaysAverage = a.EQitem.TotalLast90DaysAverage,
+                           TotalLast6MonthsCount = a.EQitem.TotalLast6MonthsCount,
+                           TotalLast6MonthsAverage = a.EQitem.TotalLast6MonthsAverage,
+                           TotalAuctionCount = a.EQitem.TotalAuctionCount,
+                           TotalAuctionAverage = a.EQitem.TotalAuctionAverage,
+                           LastSeen = a.EQitem.LastSeen
+                       })
+                       .Select(a => a.Key).ToList()
                        .OrderBy(a => a.ItemName)
                        .ToList();
                 }
@@ -48,12 +56,6 @@ namespace EQToolApis.Pages
                 return _AuctionItems;
             }
         }
-
-        public List<AuctionItem> AuctionItemsCol1 => AuctionItems.Take(AuctionItems.Count / 2).ToList();
-
-        public List<AuctionItem> AuctionItemsCol2 => AuctionItems.Skip(AuctionItems.Count / 2).ToList();
-
-
         public IActionResult OnGet()
         {
             return Page();
