0ce809a0b9ae7908c9f32067ccdea42ded067f38
Scott Lee <scott.lee@tai-software.com>
2023-07-07 18:18:56 -0700

    updates

--- EQToolApis/Pages/Index.cshtml.cs
+++ EQToolApis/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using EQToolApis.DB;
 using EQToolApis.DB.Models;
 using EQToolApis.Models;
+using EQToolShared.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,11 +10,43 @@ namespace EQToolApis.Pages
     public class IndexModel : PageModel
     {
         public readonly DBData AllData;
+        public readonly NoteableNPCCache noteableNPCCache;
         private readonly EQToolContext eQToolContext;
-        public IndexModel(DBData allData, EQToolContext eQToolContext)
+        public List<NoteableNPC> GreenNoteableNPCs = new List<NoteableNPC>();
+
+        public IndexModel(DBData allData, EQToolContext eQToolContext, NoteableNPCCache noteableNPCCache)
         {
             AllData = allData;
             this.eQToolContext = eQToolContext;
+            this.noteableNPCCache = noteableNPCCache;
+            var keyname = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("westwastes", "Scout Charisa"),
+                new KeyValuePair<string, string>("westwastes", "A Kromzek Captain"),
+                new KeyValuePair<string, string>("droga", "An angry goblin"),
+                new KeyValuePair<string, string>("droga", "Warlord Skargus")
+            };
+
+            foreach (var item in keyname)
+            {
+                var def = new NoteableNPC { EQNotableNPCId = 0, Name = item.Value };
+                if (noteableNPCCache.ServerData[(int)Servers.Green].Zones.TryGetValue(item.Key, out var npc))
+                {
+                    var n = npc.FirstOrDefault(a => a.Name == item.Value);
+                    if (n != null)
+                    {
+                        GreenNoteableNPCs.Add(n);
+                    }
+                    else
+                    {
+                        GreenNoteableNPCs.Add(def);
+                    }
+                }
+                else
+                {
+                    GreenNoteableNPCs.Add(def);
+                }
+            }
         }
 
         public ServerMessage ServerMessage { get; set; } = new ServerMessage();
