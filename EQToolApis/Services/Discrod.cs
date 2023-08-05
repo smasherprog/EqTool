529166cf66dd2a834415fc15e37914deeb7a3d4e
Scott Lee <scott.lee@tai-software.com>
2023-08-05 11:14:39 -0700

    fixed green message history

--- EQToolApis/Services/Discrod.cs
+++ EQToolApis/Services/Discrod.cs
@@ -1,6 +1,7 @@
 ﻿using EQToolApis.DB;
 using EQToolApis.DB.Models;
 using EQToolApis.Models;
+using EQToolShared.Discord;
 using EQToolShared.Enums;
 using Hangfire;
 using Microsoft.Extensions.Options;
@@ -11,8 +12,8 @@ namespace EQToolApis.Services
     public interface IDiscordService
     {
         public void Login();
-        public List<DiscordService.Message> ReadMessages(long? lastid, Servers server);
-        public List<DiscordService.Message> ReadMessageHistory(long? lastid, Servers server);
+        public List<Message> ReadMessages(long? lastid, Servers server);
+        public List<Message> ReadMessageHistory(long? lastid, Servers server);
     }
 
     public class DiscordServiceOptions
@@ -34,22 +35,6 @@ namespace EQToolApis.Services
             ChannelIds[(int)Servers.Blue] = "720860598362963998";
         }
 
-        public class LoginRequest
-        {
-            public string captcha_key { get; set; } = string.Empty;
-            public string gift_code_sku_id { get; set; } = string.Empty;
-            public string login { get; set; } = string.Empty;
-            public string login_source { get; set; } = string.Empty;
-            public string password { get; set; } = string.Empty;
-            public bool undelete { get; set; } = false;
-        }
-
-        public class LoginResponse
-        {
-            public string Token { get; set; } = string.Empty;
-            public string user_id { get; set; } = string.Empty;
-        }
-
         public void Login()
         {
             //if (loginResponse != null)
@@ -69,71 +54,6 @@ namespace EQToolApis.Services
             //{
             //    throw new Exception($"Login Failed: {res}");
             //}
-        }
-
-        public class embedFields
-        {
-            public int? Price
-            {
-                get
-                {
-                    if (name == "No Price Listed" || name.StartsWith("00") || name.Contains("00000000000000000"))
-                    {
-                        return null;
-                    }
-
-                    _ = float.TryParse(new string(name.Where(a => char.IsDigit(a) || a == '.').ToArray()), out var p);
-                    if (name.EndsWith("pp"))
-                    {
-                        if (p <= 0)
-                        {
-                            return null;
-                        }
-                        else if (p > 1000000)
-                        {
-                            return null;
-                        }
-                        return (int)p;
-                    }
-                    else if (name.EndsWith("k"))
-
-                    {
-                        var r = (int)(p * 1000);
-                        if (r <= 0)
-                        {
-                            return null;
-                        }
-                        else if (r > 1000000)
-                        {
-                            return null;
-                        }
-                        return r;
-                    }
-                    return (int)p;
-                }
-            }
-
-            public string ItemName => string.IsNullOrWhiteSpace(value) || !value.Contains("[") || !value.Contains("]")
-                        ? string.Empty
-                        : value.Substring(1, value.IndexOf("]")).Trim(']').Trim('[').Trim();
-
-            public string? name { get; set; }
-            public string? value { get; set; }
-        }
-
-        public class MessageEmbed
-        {
-            public string? title { get; set; }
-            public AuctionType AuctionType => title.StartsWith("**[ WTB ]**") ? AuctionType.WTB : (title.StartsWith("**[ WTS ]**") ? AuctionType.WTS : AuctionType.BOTH);
-            public string AuctionPerson => title[(title.LastIndexOf("**") + 2)..].Trim();
-            public DateTimeOffset timestamp { get; set; }
-            public List<embedFields>? fields { get; set; }
-        }
-
-        public class Message
-        {
-            public long id { get; set; }
-            public List<MessageEmbed>? embeds { get; set; }
         }
 
         public List<Message> ReadMessages(long? lastid, Servers server)
@@ -321,22 +241,34 @@ namespace EQToolApis.Services
             {
                 discordService.Login();
                 var lastidread = dBData.ServerData[(int)server].OrderByDescendingDiscordMessageId;
-                var messages = AddMessages(discordService.ReadMessages(lastidread, server), server);
-                if (messages.Any())
+                var possiblemessages2 = discordService.ReadMessages(lastidread, server);
+                var messages2 = AddMessages(possiblemessages2, server);
+                if (messages2.Any())
                 {
-                    var id = messages.Select(a => a.DiscordMessageId).OrderByDescending(a => a).FirstOrDefault();
+                    var id = messages2.Select(a => a.id).OrderByDescending(a => a).FirstOrDefault();
                     var oldid = dBData.ServerData[(int)server].OrderByDescendingDiscordMessageId.Value;
                     lock (dBData)
                     {
-                        dBData.ServerData[(int)server].RecentImportTimeStamp = messages.Select(a => a.TunnelTimestamp).OrderByDescending(a => a).FirstOrDefault();
+                        dBData.ServerData[(int)server].RecentImportTimeStamp = messages2.Select(a => a.TunnelTimestamp).OrderByDescending(a => a).FirstOrDefault();
                         dBData.ServerData[(int)server].OrderByDescendingDiscordMessageId = dBData.ServerData[(int)server].OrderByDescendingDiscordMessageId.HasValue
                             ? Math.Max(dBData.ServerData[(int)server].OrderByDescendingDiscordMessageId.Value, id)
                             : id;
-                        return messages.Count + $" messages added. {oldid} -> {dBData.ServerData[(int)server].OrderByDescendingDiscordMessageId}";
+                        return messages2.Count + $" messages added. {oldid} -> {dBData.ServerData[(int)server].OrderByDescendingDiscordMessageId}";
                     }
                 }
-
-                return messages.Count + " messages added";
+                else if (possiblemessages2.Any())
+                {
+                    var id = possiblemessages2.Select(a => a.id).OrderByDescending(a => a).FirstOrDefault();
+                    var oldid = dBData.ServerData[(int)server].OrderByDescendingDiscordMessageId.Value;
+                    lock (dBData)
+                    {
+                        dBData.ServerData[(int)server].OrderByDescendingDiscordMessageId = dBData.ServerData[(int)server].OrderByDescendingDiscordMessageId.HasValue
+                            ? Math.Max(dBData.ServerData[(int)server].OrderByDescendingDiscordMessageId.Value, id)
+                            : id;
+                        return $"0 messages added. {oldid} -> {dBData.ServerData[(int)server].OrderByDescendingDiscordMessageId}";
+                    }
+                }
+                return messages2.Count + " messages added";
             }
 
             [AutomaticRetry(Attempts = 0), DisplayName("ReadPastMessages {0}")]
@@ -344,8 +276,9 @@ namespace EQToolApis.Services
             {
                 discordService.Login();
                 var lastidread = dBData.ServerData[(int)server].OrderByDiscordMessageId;
-                var messages = AddMessages(discordService.ReadMessageHistory(lastidread, server), server);
-                if (messages.Any())
+                var possiblemessages = discordService.ReadMessages(lastidread, server);
+                var messages = AddMessages(possiblemessages, server);
+                if (possiblemessages.Any())
                 {
                     var id = messages.Select(a => a.DiscordMessageId).OrderBy(a => a).FirstOrDefault();
                     lock (dBData)
---
+++ EQToolShared/Discord/Models.cs
@@ -0,0 +1,90 @@
+﻿using EQToolShared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EQToolShared.Discord
+{
+
+    public class LoginRequest
+    {
+        public string captcha_key { get; set; } = string.Empty;
+        public string gift_code_sku_id { get; set; } = string.Empty;
+        public string login { get; set; } = string.Empty;
+        public string login_source { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+        public bool undelete { get; set; } = false;
+    }
+
+    public class LoginResponse
+    {
+        public string Token { get; set; } = string.Empty;
+        public string user_id { get; set; } = string.Empty;
+    }
+
+    public class embedFields
+    {
+        public int? Price
+        {
+            get
+            {
+                if (name == "No Price Listed" || name.StartsWith("00") || name.Contains("00000000000000000"))
+                {
+                    return null;
+                }
+
+                _ = float.TryParse(new string(name.Where(a => char.IsDigit(a) || a == '.').ToArray()), out var p);
+                if (name.EndsWith("pp"))
+                {
+                    if (p <= 0)
+                    {
+                        return null;
+                    }
+                    else if (p > 1000000)
+                    {
+                        return null;
+                    }
+                    return (int)p;
+                }
+                else if (name.EndsWith("k"))
+
+                {
+                    var r = (int)(p * 1000);
+                    if (r <= 0)
+                    {
+                        return null;
+                    }
+                    else if (r > 1000000)
+                    {
+                        return null;
+                    }
+                    return r;
+                }
+                return (int)p;
+            }
+        }
+
+        public string ItemName => string.IsNullOrWhiteSpace(value) || !value.Contains("[") || !value.Contains("]")
+                    ? string.Empty
+                    : value.Substring(1, value.IndexOf("]")).Trim(']').Trim('[').Trim();
+
+        public string name { get; set; }
+        public string value { get; set; }
+    }
+
+    public class MessageEmbed
+    {
+        public string title { get; set; }
+        public AuctionType AuctionType => title.StartsWith("**[ WTB ]**") ? AuctionType.WTB : (title.StartsWith("**[ WTS ]**") ? AuctionType.WTS : AuctionType.BOTH);
+        public string AuctionPerson => title.Substring(title.LastIndexOf("**") + 2).Trim();
+        public DateTimeOffset timestamp { get; set; }
+        public List<embedFields> fields { get; set; }
+    }
+
+    public class Message
+    {
+        public long id { get; set; }
+        public List<MessageEmbed> embeds { get; set; }
+    }
+
+}
