e26f6757f669d5f89b955ec19d5773e7d61ee084
Scott Lee <scott.lee@tai-software.com>
2024-02-01 09:01:07 -0800

    updated PQ instanced zone names

--- EQToolShared/Map/ZoneParser.cs
+++ EQToolShared/Map/ZoneParser.cs
@@ -2038,7 +2038,7 @@ namespace EQToolShared.Map
             ZoneNameMapper.Add("blackburrow", "blackburrow");
             ZoneNameMapper.Add("gorge of king xorbb", "beholder");
             ZoneNameMapper.Add("plane of hate", "hateplane");
-            ZoneNameMapper.Add("plane of hate (Instance)", "hateplane");
+            ZoneNameMapper.Add("plane of hate (Instanced)", "hateplane");
             ZoneNameMapper.Add("west commonlands", "commons");
             ZoneNameMapper.Add("north qeynos", "qeynos2");
             ZoneNameMapper.Add("cobalt scar", "cobaltscar");
@@ -2046,14 +2046,14 @@ namespace EQToolShared.Map
             ZoneNameMapper.Add("paineel", "paineel");
             ZoneNameMapper.Add("north freeport", "freportn");
             ZoneNameMapper.Add("nagafen's lair", "soldungb");
-            ZoneNameMapper.Add("nagafen's lair (Instance)", "soldungb");
+            ZoneNameMapper.Add("nagafen's lair (Instanced)", "soldungb");
             ZoneNameMapper.Add("runnyeye citadel", "runnyeye");
             ZoneNameMapper.Add("frontier mountains", "frontiermtns");
             ZoneNameMapper.Add("the city of mist", "citymist");
             ZoneNameMapper.Add("west freeport", "freportw");
             ZoneNameMapper.Add("butcherblock mountains", "butcher");
             ZoneNameMapper.Add("permafrost caverns", "permafrost");
-            ZoneNameMapper.Add("permafrost caverns (Instance)", "permafrost");
+            ZoneNameMapper.Add("permafrost caverns (Instanced)", "permafrost");
             ZoneNameMapper.Add("the hole", "hole");
             ZoneNameMapper.Add("qeynos hills", "qeytoqrg");
             ZoneNameMapper.Add("arena", "arena");
@@ -2067,7 +2067,7 @@ namespace EQToolShared.Map
             ZoneNameMapper.Add("infected paw", "paw");
             ZoneNameMapper.Add("lair of the splitpaw", "paw");
             ZoneNameMapper.Add("plane of air", "airplane");
-            ZoneNameMapper.Add("plane of air (Instance)", "airplane");
+            ZoneNameMapper.Add("plane of air (Instanced)", "airplane");
             ZoneNameMapper.Add("southern felwithe", "felwitheb");
             ZoneNameMapper.Add("velketor's labyrinth", "velketor");
             ZoneNameMapper.Add("cabilis west", "cabwest");
@@ -2084,7 +2084,7 @@ namespace EQToolShared.Map
             ZoneNameMapper.Add("dreadlands", "dreadlands");
             ZoneNameMapper.Add("south qeynos", "qeynos");
             ZoneNameMapper.Add("plane of fear", "fearplane");
-            ZoneNameMapper.Add("plane of fear (Instance)", "fearplane");
+            ZoneNameMapper.Add("plane of fear (Instanced)", "fearplane");
             ZoneNameMapper.Add("rathe mountains", "rathemtn");
             ZoneNameMapper.Add("the wakening lands", "wakening");
             ZoneNameMapper.Add("southern desert of ro", "sro");
