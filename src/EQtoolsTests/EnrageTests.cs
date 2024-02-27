085c8d2619f3b644dd75eeffa89ff788b31cc02c
Scott Lee <scott.lee@tai-software.com>
2024-02-26 20:19:46 -0800

    updates

--- src/EQtoolsTests/EnrageTests.cs
+++ src/EQtoolsTests/EnrageTests.cs
@@ -1,8 +1,5 @@
 ﻿using Autofac;
-using EQTool.Models;
 using EQTool.Services;
-using EQTool.ViewModels;
-using EQToolShared.Enums;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace EQToolTests
@@ -20,32 +17,8 @@ namespace EQToolTests
         public void Parse1()
         {
             var service = container.Resolve<EnrageParser>();
-            var player = container.Resolve<ActivePlayer>();
-            player.Player = new PlayerInfo
-            {
-                Level = 54,
-                PlayerClass = PlayerClasses.Cleric,
-                Zone = "templeveeshan"
-            };
-
             var d = service.EnrageCheck("Cekenar has become ENRAGED.");
             Assert.AreEqual(d.NpcName, "Cekenar");
-        }
-
-        [TestMethod]
-        public void ParseNotInzone()
-        {
-            var service = container.Resolve<EnrageParser>();
-            var player = container.Resolve<ActivePlayer>();
-            player.Player = new PlayerInfo
-            {
-                Level = 54,
-                PlayerClass = PlayerClasses.Cleric,
-                Zone = "swampofnohope"
-            };
-
-            var d = service.EnrageCheck("Cekenar has become ENRAGED.");
-            Assert.IsNull(d);
         }
     }
 }
