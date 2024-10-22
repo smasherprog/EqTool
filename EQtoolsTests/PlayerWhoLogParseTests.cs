40923a357facf00be8814e871c0260880628288b
Scott Lee <smasherprog@gmail.com>
2024-10-21 18:59:44 -0700

    Forced a build

--- EQtoolsTests/PlayerWhoLogParseTests.cs
+++ EQtoolsTests/PlayerWhoLogParseTests.cs
@@ -1,21 +1,20 @@
 ﻿using Autofac;
-using EQTool.Services.Parsing;
 using EQToolShared.Enums;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace EQtoolsTests
 {
     [TestClass]
-    public class PlayerParsingTests : BaseTestClass
-    { 
-        public PlayerParsingTests()
-        { 
+    public class PlayerWhoLogParse : BaseTestClass
+    {
+        public PlayerWhoLogParse()
+        {
         }
 
         [TestMethod]
         public void AnonTest()
         {
-            var service = container.Resolve<PlayerWhoLogParse>();
+            var service = container.Resolve<EQTool.Services.Parsing.PlayerWhoLogParse>();
             var guess = service.ParsePlayerInfo("[ANONYMOUS] Rundorig  <The Drift>");
             Assert.IsNotNull(guess);
             Assert.AreEqual("Rundorig", guess.Name);
@@ -27,7 +26,7 @@ namespace EQtoolsTests
         [TestMethod]
         public void AFKAnonTest()
         {
-            var service = container.Resolve<PlayerWhoLogParse>();
+            var service = container.Resolve<EQTool.Services.Parsing.PlayerWhoLogParse>();
             var guess = service.ParsePlayerInfo("AFK [ANONYMOUS] Rundorig  <The Drift>");
             Assert.IsNotNull(guess);
             Assert.AreEqual("Rundorig", guess.Name);
@@ -39,7 +38,7 @@ namespace EQtoolsTests
         [TestMethod]
         public void ClericTest60()
         {
-            var service = container.Resolve<PlayerWhoLogParse>();
+            var service = container.Resolve<EQTool.Services.Parsing.PlayerWhoLogParse>();
             var guess = service.ParsePlayerInfo("[60 High Priest] Dany (High Elf) <The Drift>");
             Assert.IsNotNull(guess);
             Assert.AreEqual("Dany", guess.Name);
@@ -51,7 +50,7 @@ namespace EQtoolsTests
         [TestMethod]
         public void WarTest60()
         {
-            var service = container.Resolve<PlayerWhoLogParse>();
+            var service = container.Resolve<EQTool.Services.Parsing.PlayerWhoLogParse>();
             var guess = service.ParsePlayerInfo("[58 Myrmidon] Bdain (Barbarian) <The Drift> LFG");
             Assert.IsNotNull(guess);
             Assert.AreEqual("Bdain", guess.Name);
@@ -63,7 +62,7 @@ namespace EQtoolsTests
         [TestMethod]
         public void Level1Bard()
         {
-            var service = container.Resolve<PlayerWhoLogParse>();
+            var service = container.Resolve<EQTool.Services.Parsing.PlayerWhoLogParse>();
             var guess = service.ParsePlayerInfo("[1 Bard] Bdain (Barbarian) <The Drift> LFG");
             Assert.IsNotNull(guess);
             Assert.AreEqual("Bdain", guess.Name);
@@ -75,7 +74,7 @@ namespace EQtoolsTests
         [TestMethod]
         public void Level1BardNoGuild()
         {
-            var service = container.Resolve<PlayerWhoLogParse>();
+            var service = container.Resolve<EQTool.Services.Parsing.PlayerWhoLogParse>();
             var guess = service.ParsePlayerInfo("[1 Bard] Bdain (Barbarian) LFG");
             Assert.IsNotNull(guess);
             Assert.AreEqual("Bdain", guess.Name);
@@ -87,7 +86,7 @@ namespace EQtoolsTests
         [TestMethod]
         public void AllAnon()
         {
-            var service = container.Resolve<PlayerWhoLogParse>();
+            var service = container.Resolve<EQTool.Services.Parsing.PlayerWhoLogParse>();
             var guess = service.ParsePlayerInfo("[ANONYMOUS] Rundorig");
             Assert.IsNotNull(guess);
             Assert.AreEqual("Rundorig", guess.Name);
@@ -99,7 +98,7 @@ namespace EQtoolsTests
         [TestMethod]
         public void AllAnonLfg()
         {
-            var service = container.Resolve<PlayerWhoLogParse>();
+            var service = container.Resolve<EQTool.Services.Parsing.PlayerWhoLogParse>();
             var guess = service.ParsePlayerInfo("[ANONYMOUS] Rundorig  LFG");
             Assert.IsNotNull(guess);
             Assert.AreEqual("Rundorig", guess.Name);
@@ -111,7 +110,7 @@ namespace EQtoolsTests
         [TestMethod]
         public void GuildMOTD()
         {
-            var service = container.Resolve<PlayerWhoLogParse>();
+            var service = container.Resolve<EQTool.Services.Parsing.PlayerWhoLogParse>();
             var guess = service.ParsePlayerInfo("GUILD MOTD: <<>> things go here [ test ] ");
             Assert.IsNull(guess);
         }
@@ -119,7 +118,7 @@ namespace EQtoolsTests
         [TestMethod]
         public void TestRandomCharacters()
         {
-            var service = container.Resolve<PlayerWhoLogParse>();
+            var service = container.Resolve<EQTool.Services.Parsing.PlayerWhoLogParse>();
             var guess = service.ParsePlayerInfo(" [TESTER 10:00PM] [VIEW] bla bla bla bla bla ' ");
             Assert.IsNull(guess);
         }
@@ -127,7 +126,7 @@ namespace EQtoolsTests
         [TestMethod]
         public void TestRandomCharacters1()
         {
-            var service = container.Resolve<PlayerWhoLogParse>();
+            var service = container.Resolve<EQTool.Services.Parsing.PlayerWhoLogParse>();
             var guess = service.ParsePlayerInfo(" [ TESTER 10:00PM] [VIEW] bla bla bla bla bla ' ");
             Assert.IsNull(guess);
         }
