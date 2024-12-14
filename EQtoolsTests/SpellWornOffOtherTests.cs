3ca232c319bb7a3cd857801985e31a5c7d272a6a
scott lee <smasherprog@gmail.com>
2024-12-13 20:13:15 -0800

    update tests

--- EQtoolsTests/SpellWornOffOtherTests.cs
+++ EQtoolsTests/SpellWornOffOtherTests.cs
@@ -17,10 +17,14 @@ namespace EQtoolsTests
     public class SpellWornOffOtherTests : BaseTestClass
     {
         private readonly SpellWornOffOtherParser parser;
+        private readonly LogParser logParser;
+        private readonly SpellWindowViewModel spellWindowViewModel;
 
         public SpellWornOffOtherTests()
         {
-            parser = container.Resolve<SpellWornOffOtherParser>();
+            parser = container.Resolve<SpellWornOffOtherParser>(); 
+            spellWindowViewModel = container.Resolve<SpellWindowViewModel>();
+            logParser = container.Resolve<LogParser>();
         }
 
         [TestMethod]
@@ -34,6 +38,19 @@ namespace EQtoolsTests
             Assert.AreEqual(now, e.TimeStamp);
             Assert.AreEqual(line, e.Line);
             Assert.AreEqual("Venom of the Snake", e.SpellName);
+        }
+
+        [TestMethod]
+        public void VenomOfTheSnakeViewModel()
+        {
+            this.player.Player.PlayerClass = EQToolShared.Enums.PlayerClasses.Necromancer;
+            this.player.Player.Level = 53;
+            logParser.Push("Someone has been poisoned.", DateTime.Now);
+            var dteffect = spellWindowViewModel.SpellList.FirstOrDefault(a => a.SpellViewModelType == SpellViewModelType.Spell && a.Name == "Envenomed Bolt") as SpellViewModel;
+            Assert.AreEqual(dteffect.TotalDuration.TotalSeconds, 41, 2);
+            logParser.Push("Your Envenomed Bolt spell has worn off.", DateTime.Now.AddSeconds(40));
+            dteffect = spellWindowViewModel.SpellList.FirstOrDefault(a => a.SpellViewModelType == SpellViewModelType.Spell && a.Name == "Envenomed Bolt") as SpellViewModel;
+            Assert.IsNull(dteffect);
         }
 
         [TestMethod]
