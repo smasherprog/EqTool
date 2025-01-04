7393df06b84ed0d4c382412a816d93f6601e7f47
Scott Lee <smasherprog@gmail.com>
2025-01-04 07:50:16 -0800

    Small code cleanup

---
+++ EQTool/Services/Handlers/QuakeHandler.cs
@@ -0,0 +1,25 @@
+﻿using EQTool.Models;
+
+namespace EQTool.Services.Handlers
+{
+    public class QuakeHandler : BaseHandler
+    {
+        private readonly PigParseApi pigParseApi;
+
+        public QuakeHandler(BaseHandlerData baseHandlerData, PigParseApi pigParseApi) : base(baseHandlerData)
+        {
+            logEvents.QuakeEvent += LogEvents_QuakeEvent;
+            this.pigParseApi = pigParseApi;
+        }
+
+        private void LogEvents_QuakeEvent(object sender, QuakeEvent e)
+        {
+            var server = activePlayer?.Player?.Server;
+            if (server.HasValue)
+            {
+                pigParseApi.SendQuake(server.Value);
+            }
+        }
+
+    }
+}
