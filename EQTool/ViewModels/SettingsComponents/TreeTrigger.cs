079ee7af23a455dbcbfdf3eddf509fdf7403ede9
scott lee <smasherprog@gmail.com>
2025-04-27 03:08:24 -0700

    Fixed trigger name and made validation faster

--- EQTool/ViewModels/SettingsComponents/TreeTrigger.cs
+++ EQTool/ViewModels/SettingsComponents/TreeTrigger.cs
@@ -7,6 +7,15 @@ namespace EQTool.ViewModels.SettingsComponents
         public TreeTrigger(TriggerViewModel userTrigger, TreeViewItemBase parent) : base(parent)
         {
             this.Trigger = userTrigger;
+            this.Trigger.PropertyChanged += Trigger_PropertyChanged;
+        }
+
+        private void Trigger_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(Trigger.TriggerName))
+            {
+                this.OnPropertyChanged(nameof(Name));
+            }
         }
 
         public TriggerViewModel Trigger { get; set; }
