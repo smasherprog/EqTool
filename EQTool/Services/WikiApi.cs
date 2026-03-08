2cf5730cb7fbfe8e5acc5f9fc26e75ebe136cf10
Scott Lee <smasherprog@gmail.com>
2026-03-08 10:43:20 -0700

    fixing

--- EQTool/Services/WikiApi.cs
+++ EQTool/Services/WikiApi.cs
@@ -1,4 +1,7 @@
 ﻿using EQTool.ViewModels;
+using EQToolShared;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EQTool.Services
 {
@@ -28,6 +31,69 @@ namespace EQTool.Services
             }
 
             return string.Empty;
+        }
+
+        public class DisambigData
+        {
+            public string Name { get; set; }
+            public string ZonePart { get; set; }
+        }
+
+        public static string Disambig(string response, string zone)
+        {
+            var splits = response.Split('\n').Where(a => !string.IsNullOrWhiteSpace(a)).Select(a => a.Trim()).Where(a => !string.IsNullOrWhiteSpace(a)).Where(a => a.Contains("Disambig")).ToList();
+            var data = new List<DisambigData>();
+            foreach (var item in splits)
+            {
+                var r = new DisambigData();
+                var indexof = item.IndexOf("[[");
+                if (indexof == -1)
+                {
+                    continue;
+                }
+                var name = item.Substring(indexof + 2);
+                indexof = name.IndexOf("]]");
+                if (indexof == -1)
+                {
+                    continue;
+                }
+                name = name.Substring(0, indexof);
+                r.Name = name?.Trim();
+                r.ZonePart = r.Name;
+                data.Add(r);
+                indexof = name.IndexOf("(");
+                if (indexof == -1)
+                {
+                    continue;
+                }
+                var zonefromdata = name.Substring(indexof + 1);
+                indexof = zonefromdata.IndexOf(")");
+                if (indexof == -1)
+                {
+                    continue;
+                }
+                name = zonefromdata.Substring(0, indexof);
+                r.ZonePart = name;
+            }
+            zone = zone?.ToLower() ?? string.Empty;
+            var zonnamemapper = Zones.ZoneNameMapper.FirstOrDefault(a => a.Value == zone);
+
+            foreach (var item in data)
+            {
+                if (item.ZonePart.Contains(zone))
+                {
+                    return item.Name;
+                }
+                else if (!string.IsNullOrWhiteSpace(zonnamemapper.Key) && item.ZonePart.ToLower().Contains(zonnamemapper.Key))
+                {
+                    return item.Name;
+                }
+            }
+
+            data = data.Where(a => !Zones.ZoneNames.Any(b => a.ZonePart.ToLower().Contains(b))).ToList();
+            data = data.Where(a => !Zones.ZoneWhoMapper.Keys.Any(b => a.ZonePart.ToLower().Contains(b.ToLower()))).ToList();
+            data = data.Where(a => !Zones.ZoneNameMapper.Keys.Any(b => a.ZonePart.ToLower().Contains(b.ToLower()))).ToList();
+            return data.FirstOrDefault()?.Name;
         }
     }
 }
