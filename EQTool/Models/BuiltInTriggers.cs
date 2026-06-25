8dfb0f1d4e96d5236ae9a3b1242255ea41be8d42
Elliott Jackson <elliott.w.jackson@gmail.com>
2026-06-24 21:56:38 -0700

    updated tells you trigger

--- EQTool/Models/BuiltInTriggers.cs
+++ EQTool/Models/BuiltInTriggers.cs
@@ -128,8 +128,8 @@ namespace EQTool.Models
         public static Trigger CreateTellsYou()
         {
             // search text to use after {c} macro is added to the regex engine
-            // return Build("builtin:tells-you", "Tells You", "{name}(tells you, '| -> {c}: )(?!Attacking)", true, "{name} sent a tell", "{name} sent a tell");
-            return Build("builtin:tells-you", "Tells You", "{name}(tells you, '| ->)(?!Attacking)", true, "{name} sent a tell", "{name} sent a tell");
+            // return Build("builtin:tells-you", "Tells You", "{name}(tells you, '| -> {c}: )(?!I'll give you|Attacking |Welcome to my bank|Come back soon)", true, "{name} sent a tell", "{name} sent a tell");
+            return Build("builtin:tells-you", "Tells You", "{name}(tells you, '| ->)(?!I'll give you|Attacking |Welcome to my bank|Come back soon)", true, "{name} sent a tell", "{name} sent a tell");
         }
         public static Trigger CreateEnraged()
         {
